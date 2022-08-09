1602ed2d48e77e7058792707ec29dddb2a9346c3
NguyenPhuc <phuctbdt2000@gmail.com>
2022-08-09 15:35:41 +0700

    first commit

--- app/src/main/java/com/misa/fresher/signup/SignUpActivity.kt
+++ app/src/main/java/com/misa/fresher/signup/SignUpActivity.kt
@@ -1,4 +1,4 @@
-package com.misa.fresher
+package com.misa.fresher.signup
 
 import android.content.Context
 import android.content.Intent
@@ -9,26 +9,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.LayoutInflater
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.misa.fresher.databinding.ActivitySignUpBinding
+import com.misa.fresher.login.LoginActivity
+import com.misa.fresher.login.LoginPresenter
+import com.misa.fresher.login.LoginTest
 import com.misa.fresher.model.User
 import com.misa.fresher.retrofit.ApiHelper
 import com.misa.fresher.retrofit.ApiInterface
+import com.misa.fresher.showToast
 import kotlinx.coroutines.*
 import java.lang.Exception
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), SignUpContract.View {
     private val binding: ActivitySignUpBinding by lazy { getInflater(layoutInflater) }
     val getInflater: (LayoutInflater) -> ActivitySignUpBinding
         get() = ActivitySignUpBinding::inflate
-
+    private var mSignUpPresenter: SignUpPresenter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        initPresenter()
         binding.btnSignUp.setOnClickListener {
-            signUpAccount(intent)
+            checkValid()
         }
         binding.tvLogin.setOnClickListener {
             onBackPressed()
@@ -40,46 +44,28 @@ class SignUpActivity : AppCompatActivity() {
      *@author:NCPhuc
      *@date:3/22/2022
      **/
-    private fun signUpAccount(intent: Intent) {
+    private fun checkValid() {
         val user = binding.tietSignUpUserName.text.toString()
         val pass = binding.tietSignUpPassword.text.toString()
         val rePass = binding.tietReEnterPassword.text.toString()
+        val fullName = binding.tietFullName.text.toString()
         if (user.isEmpty()) {
             application.showToast("Tài khoản không được để trống!")
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(user).matches()) {
-            application.showToast("Tài khoản không đúng định dạng email!")
         } else if (pass.isEmpty()) {
             application.showToast("Mật khẩu không được bỏ trống!")
         } else if (rePass.isEmpty()) {
             application.showToast("Mật khẩu nhập lại không được bỏ trống!")
         } else if (pass != rePass) {
             application.showToast("Mật khẩu không khớp nhau!")
-        } else if (!checkForInternet(this)) {
+        } else if (fullName.isEmpty()) {
+            application.showToast("Họ và tên không được bỏ trống!")
+        }
+        else if (!checkForInternet(this)) {
             application.showToast("Không có kết nối mạng")
         } else {
-            val userInfor = User(user, pass)
-            val resIn = ApiHelper.getInstance().create(ApiInterface::class.java)
-            binding.flProgressBar.isVisible=true
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    val signUp = resIn.signUp(userInfor)
-                    if (signUp.isSuccessful && signUp.body() != null) {
-                        withContext(Dispatchers.Main)
-                        {
-                            application.showToast("Đăng ký tài khoản thành công")
-                        }
-                        startActivity(intent)
-                    } else {
-                        withContext(Dispatchers.Main) {
-                            application.showToast(signUp.message())
-                        }
-                    }
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        application.showToast(e.message.toString())
-                    }
-                }
-            }
+            val userInfor = User(0,user, pass,fullName)
+            binding.flProgressBar.isVisible = true
+            mSignUpPresenter?.signUp(userInfor)
         }
     }
 
@@ -112,5 +98,28 @@ class SignUpActivity : AppCompatActivity() {
             @Suppress("DEPRECATION") return networkInfo.isConnected
         }
 
+    }
+
+    private fun initPresenter() {
+        mSignUpPresenter = SignUpPresenter().also { it.attach(this) }
+    }
+
+    override fun signUpSuccess() {
+        val intent = Intent(this, LoginTest::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+    override fun showProgressBar() {
+        binding.flProgressBar.isGone = true
+    }
+
+    override fun showErrorMessage(string: String) {
+        application?.showToast(string)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSignUpPresenter?.detach()
     }
 }
